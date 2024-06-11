58ff8f8baf6c981ce1289c2293bdd47ce9b26c0b
andulval <mzadlo@interia.pl>
2024-06-11 20:08:24 +0200

    user change password (logged in) and user update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,16 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const filteredObj = (body, ...allowedFilters) => {
+  const result = {};
+  allowedFilters.forEach((filter) => {
+    if (body.hasOwnProperty(filter)) {
+      result[filter] = body[filter];
+    }
+  });
+  return result;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -15,3 +26,30 @@ exports.getUser = (req, res) => {
     .status(500)
     .json({ status: 'error', message: 'This route is not yet defined!' });
 };
+
+exports.updateUserData = catchAsync(async (req, res, next) => {
+  //update information about user - by user himself
+  //1) error if user POST password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route doesnt change password, use /updatePassword',
+        400,
+      ),
+    );
+  }
+
+  //2) update user's data
+  const filteredBody = filteredObj(req.body, 'name', 'email');
+  const updateUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updateUser,
+    },
+  });
+});
--- utils/email.js
+++ utils/email.js
@@ -1,11 +1,29 @@
 const nodemailer = require('nodemailer');
 
-const sendEmail = options =>{
-    //1 create transporter
-    const transporter = nodemailer.createTransport({
-        host: 'Gmail',
-        auth: {
+const sendEmail = async (options) => {
+  //1 create transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: false,
+    logger: true,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
 
-        }
-    })
-}
+  //2) Define email options
+  const mailOptions = {
+    from: 'Michal Zadlo <michal@zadlo.pl>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html:
+  };
+  console.log(mailOptions);
+  //send the email
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
